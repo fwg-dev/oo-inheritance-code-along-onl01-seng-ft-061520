28824155090d43c7124d7cd672f5c4db72f64092
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 06:19:05 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,9 @@
 require_relative "./vehicle.rb"
-class Car
-  attr_accessor :wheel_size, :wheel_number
-
-  def initialize(wheel_size, wheel_number)
-    @wheel_size = "small"
-    @wheel_number = number
-  end
+class Car < Vehicle
+  # attr_accessor :wheel_size, :wheel_number
+  # 
+  # def initialize(wheel_size, wheel_number)
+  #   @wheel_size = "small"
+  #   @wheel_number = number
+  # end
 end
